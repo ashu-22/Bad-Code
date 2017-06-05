718a4fb8cff088ab5e86edd1e343fdb7fca62f94
Ashish Singh Chauhan <ashishsinghchauhan098@gmail.com>
2017-06-06 00:33:03 +0530

    Solution.java
    I have created Solution class in java

---
+++ Solution.java
@@ -0,0 +1,17 @@
+public class Solution{
+public static void main(String[] args)
+{
+ArrayList<list> list = new ArrayList<list>();
+al.add(1);
+al.add(2);
+al.add(3);
+al.add(4);
+al.add(5);
+al.add(6);
+al.add(7);
+al.add(8);
+al.add(9);
+al.add(10);
+System.out.println("ArrayList elements: list");
+}
+}
