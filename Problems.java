d98ad99fc47b67a56d715dee3318a3e1a0b9ca43
Ashish Singh Chauhan <ashishsinghchauhan098@gmail.com>
2017-06-07 03:54:06 +0530

    Problems.java
    Another bad code in java.

---
+++ Problems.java
@@ -0,0 +1,17 @@
+public class Solution
+{
+public static void main(String[] args)
+{
+int n;
+n = 10;
+if(n < 1)
+{
+System.out.println("false");
+}
+else
+{
+System.out.println("True");
+}
+}
+}
+}
