2fc7246befc73f39e5510d94d522d178b0fb5a95
We are Legion! <ashishsinghchauhan098@gmail.com>
2017-06-17 23:10:01 +0530

    Java Program
    Java Program

---
+++ Java Program.h
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.List;
+import java.util.Map;
+public class Solution
+{
+public static void main(String[] args)
+{
+String str = new String();
+HashSet<Set> hs = new HashSet<String>();
+hs.add("Ravi");
+hs.add("Manish");
+hs.add("Renu");
+hs.add("Vijay");
+hs.add("Meenu");
+System.out.println("hs.next()");
+}
+List l = new ArrayList<list>();
+list.add("1");
+list.add("2");
+list.add("3");
+list.add("4");
+list.add("5");
+sortList()
+{
+System.out.println("list is: "+ sort);
+}
+HashMap map = new HashMap();
+hm.put("Ashish", "1");
+hm.put("Singh" , "2");
+hm.put("Chauhan", "3");
+System.out.println("Hashmap of list is: " + map);
+}
+}
